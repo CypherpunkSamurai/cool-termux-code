7b4c8019423a3214915cd35fdcae586eeb4f6ce4
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2020-08-16 01:17:16 +0530

    Update acodefree.sh

--- acodefree.sh
+++ acodefree.sh
@@ -1,4 +1,4 @@
-export code_source=$1
+code_source=$1
 
 echo Running Acode...
 
@@ -8,6 +8,6 @@ mkdir ~/storage/shared/.acode_tmp/ -p
 
 cp $code_source ~/storage/shared/.acode_tmp/ -f
 
-export code_source=$(basename $code_source)
+code_source=$(basename $code_source)
 
 am start --user 0 -n com.foxdebug.acodefree/com.foxdebug.acodefree.MainActivity -a android.intent.action.SEND -t text/plain -d "file:///sdcard/.acode_tmp/"$code_source
